f541cc606f9b4efbfe0c44e25328ea9b97e06a11
Adeilsoara <adeilsonaragao@hotmail.com>
2020-06-27 09:08:59 -0300

    Criação do relatório(informática por escolas privadas)

--- relatorio.php
+++ relatorio.php
@@ -34,6 +34,17 @@
         <!-- <img src="icons/icons/trash-fill.svg"> -->
       </td>
     </tr>
+
+
+    <tr>
+      <td>Escola Privada</td>
+      <td>Informática</td>
+    
+      <td>
+        <a href="informatica_privada.php" target="_blank" class="btn btn-sm btn-warning">Gerar</a>
+        <!-- <img src="icons/icons/trash-fill.svg"> -->
+      </td>
+    </tr>
  
   </table>
 
