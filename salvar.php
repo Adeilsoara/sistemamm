b1ca34ec34604b0a49ff438882bcc0c824bdba09
Adeilson Aragão <adeilsonaragao@hotmail.com>
2023-03-04 10:18:44 -0300

    Update salvar.php

--- salvar.php
+++ salvar.php
@@ -33,7 +33,7 @@
 	 if (mysqli_query($connection, "INSERT INTO ALUNO(nome, curso, endereco, cidade, data, idade, sexo, nomemae, nomepai, bairro, estado, telefone1, telefone2, categoriaescola, tipoconcorrencia) VALUES ('$nome', '$curso', '$endereco', '$cidade', '$data', '$idade', '$sexo', '$nomemae', '$nomepai', '$bairro', '$estado', '$telefone1', '$telefone2', '$categoriaescola', '$tipoconcorrencia')")) {
 	 	 $id = mysqli_insert_id($connection);
 	     $sql = mysqli_query($connection, "INSERT INTO NOTAS(fisica, historia, portugues, matematica, fk_idaluno) VALUES ('$fisica', '$historia', '$portugues', '$matematica', '$id')");
-	     $sql2 = mysqli_query($connection, "INSERT INTO HISTORICO (sexto_ano, setimo_ano, oitavo_ano, nono_ano, fk_idaluno) VALUES ('$sextoano', '$setimoano', '$oitavoano', '$nonoano', '$id')");
+	    /* $sql2 = mysqli_query($connection, "INSERT INTO HISTORICO (sexto_ano, setimo_ano, oitavo_ano, nono_ano, fk_idaluno) VALUES ('$sextoano', '$setimoano', '$oitavoano', '$nonoano', '$id')");*/
 	    header('location: index.php');
 	 }
 ?>
